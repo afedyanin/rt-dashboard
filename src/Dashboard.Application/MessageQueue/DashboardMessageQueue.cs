99c435d2abc45295af09a9b2bc88e8019ab8e907
Anatoly Fedyanin <anfemail@gmail.com>
2025-09-20 09:07:34 +0300

    lock

--- src/Dashboard.Application/MessageQueue/DashboardMessageQueue.cs
+++ src/Dashboard.Application/MessageQueue/DashboardMessageQueue.cs
@@ -5,6 +5,7 @@ namespace Dashboard.Application.MessageQueue
     public class DashboardMessageQueue : IMessageQueueProducer, IMessageQueueConsumer
     {
         private readonly PriorityQueue<IDashboardMessage, int> _priorityQueue;
+        private readonly object _lock = new object();
 
         public DashboardMessageQueue()
         {
@@ -12,9 +13,19 @@ namespace Dashboard.Application.MessageQueue
         }
 
         public void Produce(IDashboardMessage message, int priority)
-            => _priorityQueue.Enqueue(message, priority);
+        {
+            lock (_lock)
+            {
+                _priorityQueue.Enqueue(message, priority);
+            }
+        }
 
         public bool TryGet(out IDashboardMessage message)
-            => _priorityQueue.TryDequeue(out message, out _);
+        {
+            lock (_lock)
+            {
+                return _priorityQueue.TryDequeue(out message, out _);
+            }
+        }
     }
 }
